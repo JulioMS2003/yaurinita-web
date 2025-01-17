e3212d1bf0cb3e16373fbf10a102bb59b3937599
JulioMS2003 <julimanchay4862@gmail.com>
2025-01-17 16:54:05 -0500

    avance 1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export function App() {
+    return (
+
+        <header>
+            
+        <div>
+            <img alt="Yaurinita" src='./img/logo.png'></img>
+        </div>
+
+       <form style={{display: 'flex'}}>
+
+        <input type="text" name="clientes" id="clientes" value=""/>
+        <button>Buscar</button>
+
+       </form>
+
+       </header>
+    )
+}
