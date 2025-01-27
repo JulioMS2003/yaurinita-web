a32f6b40858c68843b4c3544b154d94525390338
JulioMS2003 <julimanchay4862@gmail.com>
2025-01-26 19:48:26 -0500

    Vercel 2.0

--- server/server.js
+++ server/server.js
@@ -3,18 +3,27 @@ const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;  
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://yaurinita.netlify.app", 
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));  
 app.use(express.json());
 
+require('dotenv').config();
+
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', 
-  password: 'Adriano2k03', 
-  database: 'yaurinita',  
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
+
 
 db.connect((err) => {
   if (err) {
@@ -24,9 +33,14 @@ db.connect((err) => {
   console.log('Conectado a la base de datos');
 });
 
+app.get('/api/get-data', (req, res) => {
+  // Lógica de tu API
+  res.json({ message: 'Datos desde EC2' });
+});
+// Ruta de ejemplo
 app.post('/buscar', (req, res) => {
-  const { clienteTerm } = req.body;  
-  console.log('Código recibido en backend:', clienteTerm); 
+  const { clienteTerm } = req.body;
+  console.log('Código recibido en backend:', clienteTerm);
 
   const query = 'SELECT * FROM clientes WHERE codigo = ?';
 
@@ -36,16 +50,17 @@ app.post('/buscar', (req, res) => {
       return res.status(500).json({ error: 'Error en la búsqueda' });
     }
 
-    console.log('Resultados de la búsqueda por código:', results);  
+    console.log('Resultados de la búsqueda por código:', results);
 
     if (results.length === 0) {
       return res.status(404).json({ message: 'No se encontraron resultados para el código ingresado' });
     }
 
-    res.json(results);  
+    res.json(results);  // Responder con los resultados de la consulta
   });
 });
 
-app.listen(port, () => {
-  console.log(`Servidor backend corriendo en http://localhost:${port}`);
+// Configurar el servidor para escuchar en todas las interfaces
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Servidor backend corriendo en http://0.0.0.0:${port}`);
 });
